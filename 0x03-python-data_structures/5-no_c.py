68583fc8acf090055e4db43a37ca3d991513d2ee
Albert James <femzein@gmail.com>
2022-12-05 07:01:31 -0800

    Python Lists

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+
+def no_c(my_string):
+    return my_string.translate({ord(c): None for c in "cC"})
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for sublist in matrix:
+        for i in range(len(sublist)):
+            print("{:d}".format(sublist[i]), end="")
+            if i is not len(sublist) - 1:
+                print(" ", end="")
+        print("")
