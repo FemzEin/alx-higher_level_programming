779b762a72db707038f50161c4b6d7c94c91bce2
Albert James <femzein@gmail.com>
2023-03-19 21:49:42 -0700

    Updates 2

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 """
-return state id given state name; SQL injection free
-parameters given to script: username, password, database, state name to match
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
-from model_state import Base, State
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # make engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    # query python instance in database state id given state name
-    state = session.query(State).filter_by(name=argv[4]).first()
-    if state:
-        print("{:d}".format(state.id))
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
     else:
         print("Not found")
-    session.close()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,7 +3,8 @@
 This script defines a City class
 to work with MySQLAlchemy ORM.
 """
-from model_state import Base, State
+
+from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
@@ -15,9 +16,8 @@ class City(Base):
         name (str): The name of the class
         state_id (int): The state the city belongs to
     """
-
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
