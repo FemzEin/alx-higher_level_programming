277fee2f0404cf1a781da5aad5f97a4af4e50334
Albert James <femzein@gmail.com>
2023-03-31 05:43:12 -0700

    Python network 1 mod

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ fetch https://alx-intranet.hbtn.io/status; display response
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
